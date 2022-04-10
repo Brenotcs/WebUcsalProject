566c93b2df3f946c31608302e4b0e9ee05f94136
Brenotcs <brenotcs@gmail.com>
2022-04-10 17:31:51 -0300

    teste implementação html

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send('<span style="color: #f00;">Hello World!</span>');
+app.get("/", (_request, response) => {
+    include_once("index.html");
 })
 
 const PORT = process.env.PORT || 5000;
