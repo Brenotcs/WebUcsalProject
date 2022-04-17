538473e6d9c7c8ef9bf41b3dfcc033f75e1e84b3
Brenotcs <brenotcs@gmail.com>
2022-04-17 14:35:59 -0300

    alterações e algumas implementações do firebase

---
+++ app.js
@@ -0,0 +1,7 @@
+var config = {
+    apiKey: "AIzaSyBjEB6kZcgzamrNNtnXiix89lhQ5MQ1oVQ",
+    authDomain: "ucsal-web-project.firebaseapp.com",
+    databaseURL: "https://ucsal-web-project-default-rtdb.firebaseio.com",
+    storageBucket: "ucsal-web-project.appspot.com",
+};
+firebase.initializeApp(config);
--- real-time-database.js
+++ real-time-database.js
@@ -1,16 +1,33 @@
-var matricula = document.getElementById('matricula');
-var password = document.getElementById('password');
-var submit = document.getElementById('submit');
+var usersList = document.getElementById('usersList');
+var nomeInput = document.getElementById('nomeInput');
+var sobrenomeInput = document.getElementById('sobrenomeInput');
+var emailInput = document.getElementById('emailInput');
+var matriculaInput = document.getElementById('matriculaInput');
+var passwordInput = document.getElementById('passwordInput');
+var submitButton = document.getElementById('submitButton');
 
-submit;addEventListener('click', function(){
-    create(matricula.ariaValueMax, password.value)
-})
+submitButton.addEventListener('click', function () {
+    create(nomeInput.value, sobrenomeInput.value, emailInput.value, matriculaInput.value, passwordInput.value);
+});
 
-function create (matri, senha) {
+function create(nome, sobrenome, email, matricula, password) {
     var data = {
-        matri: matri,
-        senha: senha
+        nome: nome,
+        sobrenome: sobrenome,
+        email: email,
+        matricula: matricula,
+        password: password
     };
 
     return firebase.database().ref().child('users').push(data);
 }
+
+firebase.database().ref('users').on('value', function (snapshot) {
+    usersList.innerHTML = '';
+    snapshot.forEach(function (item) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(item.val().nome + ': ' + item.val().sobrenome + ': ' + item.val().email + ': ' + item.val().matricula + ': ' + item.val().password));
+        usersList.appendChild(li);
+
+    });
+});
