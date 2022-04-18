c364adba5819e72555c73206711ffb1121c33a25
Brenotcs <brenotcs@gmail.com>
2022-04-17 21:13:04 -0300

    mudanças e implementação do autenticador

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-var config = {
-    apiKey: "AIzaSyBjEB6kZcgzamrNNtnXiix89lhQ5MQ1oVQ",
-    authDomain: "ucsal-web-project.firebaseapp.com",
-    databaseURL: "https://ucsal-web-project-default-rtdb.firebaseio.com",
-    storageBucket: "ucsal-web-project.appspot.com",
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+const auth = getAuth();
+auth.languageCode = 'it';
+
+(function () {
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBjEB6kZcgzamrNNtnXiix89lhQ5MQ1oVQ",
+  authDomain: "ucsal-web-project.firebaseapp.com",
+  projectId: "ucsal-web-project",
+  storageBucket: "ucsal-web-project.appspot.com",
+  messagingSenderId: "930705067896",
+  appId: "1:930705067896:web:cf6d4346ed93a030eb028d",
+  measurementId: "G-YZXSC6DSZ4"
 };
-firebase.initializeApp(config);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+})()
