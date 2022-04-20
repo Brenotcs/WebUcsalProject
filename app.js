6af5182fb9e8bbd5d8385dd51f6042e2f3c4d2e3
Brenotcs <brenotcs@gmail.com>
2022-04-20 16:26:08 -0300

    alterações de nome e pequenas mudanças

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
+
+var fb = require('firebase');
 
 const auth = getAuth();
 auth.languageCode = 'it';
@@ -10,12 +13,15 @@ auth.languageCode = 'it';
 const firebaseConfig = {
   apiKey: "AIzaSyBjEB6kZcgzamrNNtnXiix89lhQ5MQ1oVQ",
   authDomain: "ucsal-web-project.firebaseapp.com",
+  databaseURL: "https://ucsal-web-project-default-rtdb.firebaseio.com",
   projectId: "ucsal-web-project",
   storageBucket: "ucsal-web-project.appspot.com",
   messagingSenderId: "930705067896",
   appId: "1:930705067896:web:cf6d4346ed93a030eb028d",
   measurementId: "G-YZXSC6DSZ4"
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
