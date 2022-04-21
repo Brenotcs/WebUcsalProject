a07fa52a689210c691e71f38cdf913a0d98fc420
Brenotcs <brenotcs@gmail.com>
2022-04-21 20:16:27 -0300

    implementações firebase

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-import { initializeApp } from "firebase/app";
+import firebase from 'firebase/app';
+import 'firebase/firebase-auth';
+import 'firebase/firebase-firestore';
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase, ref, set } from "firebase/database";
+import firebaseConfig from './firebaseConfig';
+
+const firebase = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
 
 var fb = require('firebase');
 
 const auth = getAuth();
 auth.languageCode = 'it';
-
-(function () {
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBjEB6kZcgzamrNNtnXiix89lhQ5MQ1oVQ",
-  authDomain: "ucsal-web-project.firebaseapp.com",
-  databaseURL: "https://ucsal-web-project-default-rtdb.firebaseio.com",
-  projectId: "ucsal-web-project",
-  storageBucket: "ucsal-web-project.appspot.com",
-  messagingSenderId: "930705067896",
-  appId: "1:930705067896:web:cf6d4346ed93a030eb028d",
-  measurementId: "G-YZXSC6DSZ4"
-};
-
-
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-})()
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 var usersList = document.getElementById('usersList');
 var nomeInput = document.getElementById('nomeInput');
 var sobrenomeInput = document.getElementById('sobrenomeInput');
@@ -6,6 +9,27 @@ var matriculaInput = document.getElementById('matriculaInput');
 var passwordInput = document.getElementById('passwordInput');
 var submitButton = document.getElementById('submitButton');
 var authGoogleButton = document.getElementById('authGoogleButton');
+
+
+(function () {
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyBjEB6kZcgzamrNNtnXiix89lhQ5MQ1oVQ",
+      authDomain: "ucsal-web-project.firebaseapp.com",
+      databaseURL: "https://ucsal-web-project-default-rtdb.firebaseio.com",
+      projectId: "ucsal-web-project",
+      storageBucket: "ucsal-web-project.appspot.com",
+      messagingSenderId: "930705067896",
+      appId: "1:930705067896:web:cf6d4346ed93a030eb028d",
+      measurementId: "G-YZXSC6DSZ4"
+    };
+    
+    
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    })()
 
 submitButton.addEventListener('click', function () {
     create(nomeInput.value, sobrenomeInput.value, emailInput.value, matriculaInput.value, passwordInput.value);
